52b15f20d935eb9c7d09d80a8a10dd13ef990a6f
Curtis Fenner <cwfenner@umich.edu>
2019-05-16 22:52:46 -0400

    Develop main

--- src/c/main.c
+++ src/c/main.c
@@ -8,8 +8,9 @@
 static void print_global_usage(char const* program) {
 	fprintf(stderr,
 	        "USAGE:\n"
-	        "\t%s parse <input .smol> <output .smol.ast>\n",
-	        program);
+	        "\t%s parse <input .smol> <output .smol.ast>\n"
+	        "\t%s analyze <input .smol 1> <...> <input .smol n>\n",
+	        program, program);
 	fflush(stderr);
 }
 
@@ -19,6 +20,65 @@ static void print_parse_usage(char const* program) {
 	        "\t%s parse <input .smol> <output .smol.ast>\n",
 	        program);
 	fflush(stderr);
+}
+
+static void print_analyze_usage(char const* program) {
+	fprintf(stderr,
+	        "USAGE:\n"
+	        "\t%s analyze <input .smol 1> <...> <input .smol n>\n",
+	        program);
+	fflush(stderr);
+}
+
+static void print(ASTLexeme lexeme) {
+	fwrite(lexeme.str, 1, lexeme.length, stdout);
+}
+
+static int main_analyze(int argc, char** argv) {
+	if (argc < 3) {
+		print_analyze_usage(argc == 0 ? "smolc" : argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	int fileCount = argc - 2;
+	ASTSource* sources = (ASTSource*)malloc(sizeof(ASTSource) * fileCount);
+	for (int i = 0; i < fileCount; i++) {
+		char const* filename = argv[i + 2];
+		FILE* file = fopen(filename, "rb");
+		if (file == NULL) {
+			fprintf(stderr, "Could not open file `%s`\n", filename);
+			fflush(stderr);
+			return EXIT_FAILURE;
+		}
+
+		Blob* blob = Blob_from_file(file, filename);
+		fclose(file);
+		if (blob == NULL) {
+			fprintf(stderr, "Could not allocate memory for file `%s`.\n",
+			        filename);
+			fflush(stderr);
+			return EXIT_FAILURE;
+		}
+
+		Error parseError = (Error){0};
+		sources[i] = ASTSource_from_blob(blob, &parseError);
+		if (Error_has_problem(&parseError)) {
+			Error_render_colorful(&parseError, stderr);
+			fflush(stderr);
+			fclose(file);
+			return EXIT_FAILURE;
+		}
+	}
+
+	for (int i = 0; i < fileCount; i++) {
+		// TODO: Compile source.
+	}
+
+	for (int i = 0; i < fileCount; i++) {
+		// TODO: Free sources[i].
+	}
+	free(sources);
+	return EXIT_SUCCESS;
 }
 
 static int main_parse(int argc, char** argv) {
@@ -32,15 +92,15 @@ static int main_parse(int argc, char** argv) {
 	if (file == NULL) {
 		fprintf(stderr, "Could not open file `%s`\n", filename);
 		fflush(stderr);
-		exit(1);
+		return EXIT_FAILURE;
 	}
 
 	Blob* blob = Blob_from_file(file, filename);
+	fclose(file);
 	if (blob == NULL) {
 		fprintf(stderr, "Could not allocate memory for file `%s`.\n", filename);
 		fflush(stderr);
-		fclose(file);
-		exit(1);
+		return EXIT_FAILURE;
 	}
 
 	Error parseError = (Error){0};
@@ -49,7 +109,7 @@ static int main_parse(int argc, char** argv) {
 		Error_render_colorful(&parseError, stderr);
 		fflush(stderr);
 		fclose(file);
-		exit(1);
+		return EXIT_FAILURE;
 	}
 
 	printf("Successfully parsed!\n");
@@ -61,11 +121,11 @@ static int main_parse(int argc, char** argv) {
 	if (out == NULL) {
 		fprintf(stderr, "Could not open file `%s`\n", astfile);
 		fclose(file);
+		return EXIT_FAILURE;
 	}
 
 	for (int32_t i = 0; i < source.parse->ast_size; i++) {
-		size_t w = fwrite(&source.parse->ast_data[i], sizeof(int32_t), 1, out);
-		assert(w == 1);
+		fprintf(out, "%d\n", (int)source.parse->ast_data[i]);
 	}
 	fflush(out);
 	fclose(out);
@@ -83,6 +143,8 @@ int main(int argc, char** argv) {
 	char const* command = argv[1];
 	if (strcmp(command, "parse") == 0) {
 		return main_parse(argc, argv);
+	} else if (strcmp(command, "analyze") == 0) {
+		return main_analyze(argc, argv);
 	}
 
 	print_global_usage(argc == 0 ? "smolc" : argv[0]);
